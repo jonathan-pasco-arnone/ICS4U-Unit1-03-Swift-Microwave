d2389068df4636fa46d459774885520700158c72
Ubuntu <ubuntu@ip-172-31-31-195.ec2.internal>
2021-11-24 00:21:03 +0000

    perfection

--- Microwave.swift
+++ Microwave.swift
@@ -8,16 +8,13 @@
 //  specific quantity of a specific type of food.
 
 // Variables
-var quantity
-var quantityMultiplier
-var minutes
-var seconds
+var quantityMultiplier: Double
 
 // Constants.
-let subTime = 60
-let pizzaTime = 45
-let soupTime = 105
-let seconds = 60
+let subTime: Double = 60
+let pizzaTime: Double = 45
+let soupTime: Double = 105
+let secondsInMinute: Double = 60
 
 // Making an error case.
 enum MyError: Error {
@@ -27,38 +24,50 @@ enum MyError: Error {
 func calculatePrice() throws {
 
     // Variables.
-    var quantityMultiplier
-    var minutes
-    var seconds
-    var getQuantityMultiplier = 0.5
-    var food = ""
-    var quantityDouble = 0
+    var quantityMultiplier: Double
+    var minutes: Double
+    var seconds: Double
+    let getQuantityMultiplier = 0.5
     let maxQuantity = 4
 
     // Inputs.
     print("Are you heating a sub, pizza, or soup?: ")
-    food = readLine()! 
+    let food = readLine()!
 
     print("How many ", food, "(s) are you heating?(max:3): ")
-    quantity = readLine()
+    let quantity = readLine()
 
-    if let quantityDouble = Double(quantity!) && quantity > 0 && quantity < maxQuantity {
+    if let quantityDouble = Double(quantity!) {
+        if Int(quantityDouble) > 0 && Int(quantityDouble) < maxQuantity {
+            // Calculations.
+            quantityMultiplier = Double((quantityDouble + 1) * getQuantityMultiplier)
 
-        // Calculations.
-        if food == "sub" {
-            minutes = floor(subTime * quantityMultiplier / 60)
-            seconds = (subTime * quantityMultiplier / 60 - minutes) * 60
-        } else if food == "pizza" {
-                minutes = floor(pizzaTime * quantityMultiplier / 60)
-                seconds = (pizzaTime * quantityMultiplier / 60 - minutes) * 60
-        } else if food == "soup" {
-                minutes = floor(soupTime * quantityMultiplier / 60)
-                seconds = (soupTime * quantityMultiplier / 60 - minutes) * 60
+            if food == "sub" {
+                minutes = Double(Int(subTime * quantityMultiplier / secondsInMinute))
+                seconds = (subTime * quantityMultiplier / secondsInMinute - Double(minutes)) * secondsInMinute
+            } else if food == "pizza" {
+                    minutes = Double(Int(pizzaTime * quantityMultiplier / secondsInMinute))
+                    seconds = (pizzaTime * quantityMultiplier / secondsInMinute - Double(minutes)) * secondsInMinute
+            } else if food == "soup" {
+                    minutes = Double(Int(soupTime * quantityMultiplier / secondsInMinute))
+                    seconds = (soupTime * quantityMultiplier / secondsInMinute - Double(minutes)) * secondsInMinute
+            } else {
+                /* This throws an error out of the called function
+                and activates the catch statement.
+                */
+                throw MyError.invalidInteger("Error")
+            }
+
+            // Outputs.
+            print("It will take ", minutes, " minutes and ", seconds, " seconds to heat.")
+            print("\nDone.")
+
+        } else {
+            /* This throws an error out of the called function
+            and activates the catch statement.
+            */
+            throw MyError.invalidInteger("Error")
         }
-
-        // Outputs.
-        print("\nDone.")
-
     } else {
 
         /* This throws an error out of the called function
